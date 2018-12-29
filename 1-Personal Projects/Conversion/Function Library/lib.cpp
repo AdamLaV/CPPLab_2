a40ea0626d4fe9d54b636a4aae1f3ff1bad88253
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-12-28 17:45:00 -0800

    Update lib.cpp

--- 1-Personal Projects/Conversion/Function Library/lib.cpp
+++ 1-Personal Projects/Conversion/Function Library/lib.cpp
@@ -1,5 +1,19 @@
-/**
-  * @brief Conversion of Hex, Binary & Decimal
-  * @author Francis
-*/
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include "lib.h"
 
+using namespace std;
+
+// getBinaryValue function
+int getBinaryValue(int &value) {
+    int binaryValue;
+    while (value > 0)
+    {
+        binaryValue = value % 2;
+        cout << binaryValue;
+        value /= 2;
+
+    }
+}
