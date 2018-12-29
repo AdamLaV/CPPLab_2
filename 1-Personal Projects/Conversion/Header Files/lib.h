27e3a51643cbf5d860c9fa4eacd8fd17ac52503d
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-12-29 13:02:44 -0800

    Create lib.h

---
+++ 1-Personal Projects/Conversion/Header Files/lib.h
@@ -0,0 +1,20 @@
+#ifndef LIB_H
+#define LIB_H
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+/**
+ * @brief getBinaryValue convert indicated value to binary value
+ * @param value to convert
+ */
+void getBinaryValue(int value);
+
+/**
+ * @brief getHex convert indicated value to hex value
+ * @param value to covert
+ */
+void getHex(int value);
+#endif // LIB_H
