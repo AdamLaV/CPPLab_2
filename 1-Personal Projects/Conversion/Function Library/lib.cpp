e790cce1489b34b9a582fbfb655c16edf550f1ef
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-12-29 13:02:04 -0800

    Update lib.cpp

--- 1-Personal Projects/Conversion/Function Library/lib.cpp
+++ 1-Personal Projects/Conversion/Function Library/lib.cpp
@@ -1,19 +1,42 @@
 #include <iostream>
 #include <cmath>
-#include <algorithm>
 #include <string>
 #include "lib.h"
 
 using namespace std;
 
 // getBinaryValue function
-int getBinaryValue(int &value) {
+void getBinaryValue(int value) {
+    cout << "Binary: ";
     int binaryValue;
-    while (value > 0)
-    {
+    while (value > 0) {
         binaryValue = value % 2;
-        cout << binaryValue;
+        cout << binaryValue << " ";
         value /= 2;
-
     }
+    cout << endl;
+}
+
+// getHex function
+void getHex(int value) {
+    cout << "Hex: ";
+    int temp, i = 1, remainder;
+    char hex[50];
+    temp = value;
+
+    while (temp != 0) {
+        remainder = temp % 16;
+        if (remainder < 10) {
+            hex[i++] = remainder + 48;
+         }
+        else {
+            hex[i++] = remainder + 55;
+         }
+        temp = temp / 16;
+    }
+
+    for (int j = i; j > 0; j--) {
+        cout << hex[j];
+    }
+    cout << endl;
 }
