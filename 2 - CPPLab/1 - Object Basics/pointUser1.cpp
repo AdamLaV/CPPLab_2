5e81beaccf30c98089dc862d31916cab21d06727
Daidemang Francis <dfranc15@my.chemeketa.edu>
2019-01-07 12:52:39 -0800

    Update pointUser1.cpp

--- 2 - CPPLab/1 - Object Basics/pointUser1.cpp
+++ 2 - CPPLab/1 - Object Basics/pointUser1.cpp
@@ -1,1 +1,35 @@
-Object Basics
+#include <iostream>
+using namespace std;
+
+class Point {
+private:
+	double x;
+	double y;
+
+public:
+	Point() {
+		x = 0;
+		y = 0;
+	}
+
+	double getX() {
+		return x;
+	}
+	
+	double getY() {
+		return y;
+	}
+};
+
+int main()
+{
+	
+//Do not modify anything on or above the line below this
+//YOUR_CODE_BELOW
+
+Point p1;
+
+//YOUR_CODE_ABOVE
+//Do not modify anything on or below the line above this
+	cout << p1.getX() << " " << p1.getY() << endl;
+}
