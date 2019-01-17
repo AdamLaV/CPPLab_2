58ae1e8e94b17e4dd02b1fd19b283c365abcb207
Daidemang Francis <dfranc15@my.chemeketa.edu>
2019-01-17 15:03:49 -0800

    Update main.cpp

--- 1-Personal Projects/Euclidean Algorithm/main.cpp
+++ 1-Personal Projects/Euclidean Algorithm/main.cpp
@@ -1,1 +1,60 @@
-//
+// Euclidean Algorithm
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+/**
+ * @brief getGCD
+ * @param a
+ * @param b
+ */
+void getGCD(int a, int b);
+
+/**
+ * @brief computeReminder
+ * @param remainderValue
+ */
+void computeReminder(int largeValue, int remainderValue);
+
+int main()
+{
+    int aValue, bValue;
+    cout << "Enter Values with space between: ";
+    cin >> aValue >> bValue;
+    getGCD(aValue, bValue);
+    //cout << aValue << " " << bValue;
+}
+
+// Euclidean Algorithm function
+
+void getGCD(int a, int b){
+    int remainder = 0; // gcd;
+    if(a > b) {
+        while (a > b) {
+            a -= b;
+            remainder = a;
+            computeReminder(a, remainder);
+        }
+    }
+    else {
+        b -= a;
+        remainder = b;
+        computeReminder(b, remainder);
+    }
+
+    //cout << remainder;
+}
+
+// computeReminder function
+void computeReminder(int largeValue, int remainderValue) {
+    int gcf, temp = 0;
+    while (lar) {
+        temp = remainderValue;
+        largeValue -= remainderValue;
+        remainderValue = largeValue;
+        largeValue = temp;
+    }
+    gcf = remainderValue;
+    cout << remainderValue;
+}
