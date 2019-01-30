d582183cbb42ab879ef0d14c87e0bd418352ca3b
Daidemang Francis <dfranc15@my.chemeketa.edu>
2019-01-29 18:31:24 -0800

    Create foreachloop.cpp

---
+++ 1-Personal Projects/foreachloop.cpp
@@ -0,0 +1,14 @@
+
+    char myArr[4] = {'N', 'S', 'E', 'W'};
+    for (int ele : myArr) {
+        if(userInput == ele) {
+            if(myArr[ele] == 'N')
+                dir = NORTH;
+            else if(myArr[ele] == 'S')
+                dir = SOUTH;
+            else if(myArr[ele] == 'E')
+                dir = EAST;
+            else
+                dir = WEST;
+        }
+    }
