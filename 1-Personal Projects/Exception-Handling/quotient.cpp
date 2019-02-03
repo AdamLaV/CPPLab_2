1768f9563786cf65949fd78af7de175a3079938e
Daidemang Francis <dfranc15@my.chemeketa.edu>
2019-02-03 12:15:22 -0800

    Create quotient.cpp

---
+++ 1-Personal Projects/Exception-Handling/quotient.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // read two integers
+    int number1, number2;
+    cin >> number1 >> number2;
+    
+    cout << number1 << " / " << number2 << " is " << (number1/number2) << endl;
+    return 0;
+}
