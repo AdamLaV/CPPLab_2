4654d72db1e896e4f1de39493e9e496e13b2b239
Daidemang Francis <dfranc15@my.chemeketa.edu>
2019-02-07 13:47:22 -0800

    Update AccessArray.cpp

--- 1-Personal Projects/AccessArray.cpp
+++ 1-Personal Projects/AccessArray.cpp
@@ -1,1 +1,7 @@
-// Pointer arithmetic to get array elements
+// Use pointer arithmetic to get array elements
+
+int nums[3] = {5,6,7};
+
+cout << *nums << endl; // 5
+cout << *(nums + 1) << endl; // 6
+cout << *(nums + 2) << endl; // 7
