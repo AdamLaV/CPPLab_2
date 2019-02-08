f77b399a14e8cc56b000803b9552b8eef815ee4c
Daidemang Francis <dfranc15@my.chemeketa.edu>
2019-02-07 16:01:48 -0800

    Update ThrowAString.cpp

--- 2 - CPPLab/5 - Operators/ThrowAString.cpp
+++ 2 - CPPLab/5 - Operators/ThrowAString.cpp
@@ -1,1 +1,34 @@
-Throw A String
+#include <iostream>
+#include <exception>
+#include <cmath>
+
+using namespace std;
+
+
+//Do not modify anything on or above the line below this
+//YOUR_CODE_BELOW
+
+double mySquareRoot(double x) {
+
+    if(x < 0) {
+		throw string("Math fail");
+	}
+	return sqrt(x);
+}
+
+//YOUR_CODE_ABOVE
+//Do not modify anything on or below the line above this
+
+int main()
+{
+    double num;
+    cin >> num;
+
+    try {
+        double result = mySquareRoot(num);
+        cout << result;
+    }
+    catch(string e) {
+        cout << e;
+    }
+}
