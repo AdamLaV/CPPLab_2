c386f3a61c4a9913926ccde212f514d0ad3a8be7
Daidemang Francis <dfranc15@my.chemeketa.edu>
2019-02-20 09:56:09 -0800

    Update Count In List.cpp

--- 2 - CPPLab/7 - Templates & Vectors/Count In List.cpp
+++ 2 - CPPLab/7 - Templates & Vectors/Count In List.cpp
@@ -1,1 +1,33 @@
-//Count In List
+#include <iostream>
+
+using namespace std;
+
+
+//Do not modify anything on or above the line below this
+//YOUR_CODE_BELOW
+
+    template<typename T>
+	int countInList(T list[], int listSize, T value) {
+		int valueOccurrence = 0;
+		for(int i = 0; i < listSize; i++) {
+			if(list[i] == value)
+				valueOccurrence++;
+		}
+		return valueOccurrence;
+	}
+
+//YOUR_CODE_ABOVE
+//Do not modify anything on or below the line above this
+
+int main()
+{
+    int nums[] = {4, 1, 2, 5, 2, 6, 2};
+    char letters[] = {'A', 'B', 'A', 'B'};
+    string words[] = {"apple", "banana", "apple"};
+
+    cout << countInList(nums, 7, 2) << endl;
+    cout << countInList(letters, 4, 'A') << endl;
+    cout << countInList<string>(words, 3, "banana") << endl;
+
+    return 0;
+}
