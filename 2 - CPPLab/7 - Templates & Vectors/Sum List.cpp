9f24081769dd1f09573d1be2ba40ea71aa4f11af
Daidemang Francis <dfranc15@my.chemeketa.edu>
2019-02-20 09:35:17 -0800

    Update Sum List.cpp

--- 2 - CPPLab/7 - Templates & Vectors/Sum List.cpp
+++ 2 - CPPLab/7 - Templates & Vectors/Sum List.cpp
@@ -1,1 +1,30 @@
-//Sum List
+#include <iostream>
+
+using namespace std;
+
+
+//Do not modify anything on or above the line below this
+//YOUR_CODE_BELOW
+
+    template<typename T>
+	T sumList(T list[], int listSize) {
+		T sum {};
+		for(int i = 0; i < listSize; i++) {
+			sum += list[i];
+		}
+		return sum;
+	}
+
+//YOUR_CODE_ABOVE
+//Do not modify anything on or below the line above this
+
+int main()
+{
+    int nums[] = {4, 2, 8, 7};
+    string words[] = {"hello", "there"};
+
+    cout << sumList(nums, 4) << endl;
+    cout << sumList(words, 2) << endl;
+
+    return 0;
+}
