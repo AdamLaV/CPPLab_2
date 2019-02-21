b3bdd6b494b6118b1da4d5f2354c25993e2a3eb1
Daidemang Francis <dfranc15@my.chemeketa.edu>
2019-02-20 17:30:54 -0800

    Update Generic Sort.cpp

--- 2 - CPPLab/7 - Templates & Vectors/Generic Sort.cpp
+++ 2 - CPPLab/7 - Templates & Vectors/Generic Sort.cpp
@@ -1,1 +1,41 @@
-//Generic Sort
+#include <iostream>
+
+using namespace std;
+
+
+//Do not modify anything on or above the line below this
+//YOUR_CODE_BELOW
+
+    template<typename T>
+	int countInList(T list[], int listSize, T value) {
+		int valueOccurrence = 0;
+		for(int i = 0; i < listSize; i++) {
+			if(list[i] == value)
+				valueOccurrence++;
+		}
+		return valueOccurrence;
+	}
+
+//YOUR_CODE_ABOVE
+//Do not modify anything on or below the line above this
+
+
+int main()
+{
+    int nums[] = {4, 1, 7, 5, 8, 6, 2};
+    char letters[] = {'S', 'R', 'C', 'B'};
+
+    sortDescending(nums, 7);
+    sortDescending(letters, 4);
+
+    for(int i = 0; i < 7; i++)
+        cout << nums[i] << " ";
+    cout << endl;
+
+
+    for(int i = 0; i < 4; i++)
+        cout << letters[i] << " ";
+    cout << endl;
+
+    return 0;
+}
