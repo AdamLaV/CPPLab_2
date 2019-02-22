33b51c32d1eb63c0173882708eb5b1bc5f741258
Daidemang Francis <dfranc15@my.chemeketa.edu>
2019-02-21 18:52:19 -0800

    Update printMiddleVectorValue.cpp

--- 2 - CPPLab/7 - Templates & Vectors/printMiddleVectorValue.cpp
+++ 2 - CPPLab/7 - Templates & Vectors/printMiddleVectorValue.cpp
@@ -1,1 +1,34 @@
-//Print Middle Vector Value
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+template <class T>
+void print(const vector<T>& v) {
+    for (auto myIt = v.begin(); myIt != v.end(); ++myIt) {
+        cout << *myIt << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    vector<int> nums;
+
+
+//Do not modify anything on or above the line below this
+//YOUR_CODE_BELOW
+
+    int num;
+    //cin >> num;
+    while(num != 0) {
+        //add to the vector  
+		nums.push_back(num);
+		cin >> num;
+    }
+    //print middle item
+	cout << nums[(nums.size()/2)];
+
+//YOUR_CODE_ABOVE
+//Do not modify anything on or below the line above this
+}
