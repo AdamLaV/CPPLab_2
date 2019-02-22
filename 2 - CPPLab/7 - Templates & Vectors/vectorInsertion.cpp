3a1f651d52853875249d6d4288e99c4c756bbb02
Daidemang Francis <dfranc15@my.chemeketa.edu>
2019-02-21 19:39:03 -0800

    Update vectorInsertion.cpp

--- 2 - CPPLab/7 - Templates & Vectors/vectorInsertion.cpp
+++ 2 - CPPLab/7 - Templates & Vectors/vectorInsertion.cpp
@@ -1,1 +1,29 @@
-//Vector Insertion
+//Program to demonstrate a reverse iterator.
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main( )
+{
+    vector<string> animals {"cat", "dog", "frog", "monkey"};
+
+
+//Do not modify anything on or above the line below this
+//YOUR_CODE_BELOW
+
+    int index;
+	string newAnimal;
+	cin >> index >> newAnimal;
+	animals.insert(animals.begin() + index, newAnimal);
+	
+
+//YOUR_CODE_ABOVE
+//Do not modify anything on or below the line above this
+    for (auto myIt = animals.begin(); myIt != animals.end(); ++myIt)
+    {
+        cout << *myIt << " ";
+    }
+    cout << endl;
+
+}
