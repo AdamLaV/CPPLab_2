7eb7886ae6268108e7f9c2c678e0a5ac4e5c4e6f
Daidemang Francis <dfranc15@my.chemeketa.edu>
2019-03-19 15:40:08 -0700

    Update Char Memory.cpp

--- 2 - CPPLab/Maps & Sets/Char Memory.cpp
+++ 2 - CPPLab/Maps & Sets/Char Memory.cpp
@@ -1,1 +1,28 @@
-Char Memory
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+
+//Do not modify anything on or above the line below this
+//YOUR_CODE_BELOW
+
+set<char> letters;
+for(int i=0;i<20;i++){
+char ch;
+cin>>ch;
+letters.insert(ch);
+}
+set <char> :: iterator itr;
+for (itr = letters.begin(); itr != letters.end(); ++itr)
+{
+cout << *itr;
+}
+	
+
+//YOUR_CODE_ABOVE
+//Do not modify anything on or below the line above this
+    return 0;
+}
