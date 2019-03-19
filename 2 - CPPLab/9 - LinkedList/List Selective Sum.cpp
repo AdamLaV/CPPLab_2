9ed7ad6a78e1040272ef198c63227d1d3329ba9f
Daidemang Francis <dfranc15@my.chemeketa.edu>
2019-03-19 14:42:31 -0700

    🎉

--- 2 - CPPLab/9 - LinkedList/List Selective Sum.cpp
+++ 2 - CPPLab/9 - LinkedList/List Selective Sum.cpp
@@ -1,1 +1,35 @@
-List Selective Sum
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+
+
+//Do not modify anything on or above the line below this
+//YOUR_CODE_BELOW
+
+int selectiveSum(list <int> l)
+{
+int sum=0;
+for(auto it = l.begin(); it!=l.end();++it)
+{
+if(*it > 0)
+sum+=*it;
+}
+return sum;
+}
+
+//YOUR_CODE_ABOVE
+//Do not modify anything on or below the line above this
+int main() {
+    list<int> values {1, 2, 3};
+    cout << selectiveSum(values) << endl;
+
+    list<int> values2 {-5, 10, 0, 6};
+    cout << selectiveSum(values2) << endl;
+
+    list<int> values3 {-5};
+    cout << selectiveSum(values3) << endl;
+}
+
+
