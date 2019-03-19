091aa380f6adf1964b12205cfadd1eb82ac933c3
Daidemang Francis <dfranc15@my.chemeketa.edu>
2019-03-19 15:35:32 -0700

    Update Biendian Insertion

--- 2 - CPPLab/9 - LinkedList/Biendian Insertion.cpp
+++ 2 - CPPLab/9 - LinkedList/Biendian Insertion.cpp
@@ -1,1 +1,43 @@
-Biendian Insertion
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+
+template <class T>
+void print(const list<T>& v) {
+    for (auto myIt = v.begin(); myIt != v.end(); ++myIt) {
+        cout << *myIt << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    list<int> values;
+
+
+//Do not modify anything on or above the line below this
+//YOUR_CODE_BELOW
+
+std::list<int>::iterator it;
+  
+int arr[6];
+  
+for(int i=0;i<6;i++){
+cin>>arr[i];
+}
+for(int i=0;i<6;i++){
+if(arr[i]>=0){
+it=values.end();
+values.insert(it,arr[i]);
+}
+else{
+it=values.begin();
+values.insert(it,arr[i]);
+}
+}
+
+//YOUR_CODE_ABOVE
+//Do not modify anything on or below the line above this
+    print(values);
+}
